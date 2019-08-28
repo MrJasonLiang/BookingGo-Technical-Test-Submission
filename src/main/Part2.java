31e349289a5a320e73136e1af59a0df911069717
Jason <jasonliang222@yahoo.co.uk>
2019-08-28 16:09:37 +0100

    Added part 2 logic and converted to Maven project

---
+++ src/main/Part2.java
@@ -0,0 +1,7 @@
+package main;
+
+public class Part2 {
+	public static void main(String[] args) {
+		new RestApiController();
+	}
+}
---
+++ src/main/RestApiController.java
@@ -0,0 +1,61 @@
+package main;
+
+import static spark.Spark.*;
+
+import com.google.gson.Gson;
+
+public class RestApiController {
+	public RestApiController() {
+		get("/rides/api", (request, response) -> {
+        	response.type("application/json");
+			
+			String pickup = request.queryParams("pickup");
+        	String dropoff = request.queryParams("dropoff");
+        	String numPassengers = request.queryParams("passengers");
+        	
+        	if (pickup == null) {
+        		response.status(400);
+        		return "{\"message\": \"Required String parameter 'pickup' is not present\"}";
+        	} else if (!Utilities.locationStringValid(pickup)) {
+        		response.status(400);
+        		return "{\"message\": \"Invalid Pickup location\"}";
+        	} else if (dropoff == null) {
+        		response.status(400);
+        		return "{\"message\": \"Required String parameter 'dropoff' is not present\"}";
+        	} else if (!Utilities.locationStringValid(dropoff)) {
+        		response.status(400);
+        		return "{\"message\": \"Invalid dropoff location\"}";
+        	}
+        	
+        	SearchResult searchResult;
+        	Location pickupLocation = Utilities.stringToLocation(pickup);
+    		Location dropoffLocation = Utilities.stringToLocation(dropoff);
+    		
+        	if (numPassengers == null) {
+        		searchResult = new SearchEngine().searchRides(pickupLocation, dropoffLocation);
+        	} else if (Utilities.numPassengersValid(numPassengers)) {
+        		searchResult = new SearchEngine().searchRides(pickupLocation, dropoffLocation, Integer.parseInt(numPassengers));
+        	} else {
+        		response.status(400);
+        		return "{\"message\": \"Invalid number of passengers\"}";
+        	}
+        	
+        	return new Gson().toJson(searchResult.getCheapestRidesDescPrice());
+        });
+		
+		post("/rides/api", (request, response) -> {
+			response.status(400);
+    		return "{\"message\": \"POST operation not supported\"}";
+		});
+
+		put("/rides/api", (request, response) -> {
+			response.status(400);
+    		return "{\"message\": \"PUT operation not supported\"}";
+		});
+
+		delete("/rides/api", (request, response) -> {
+			response.status(400);
+    		return "{\"message\": \"DELETE operation not supported\"}";
+		});
+	}
+}
